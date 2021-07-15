14d409f40c89ba5b6f2a137854c88224c10b3b6f
Max Davish <mdavish@yext.com>
2021-07-15 12:36:13 -0400

    adding additional arguments for retrying after intermittent 500s

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging import error
 import os
 import requests
 import json
@@ -40,29 +41,47 @@ def get_all_topics(discourse_url: str, discourse_api_key: str) -> List:
             page_topics = rj['topic_list']['topics']
             all_topics.extend(page_topics)
             page += 1
-            response = requests.get(discourse_url, auth=auth, params={'page': page})
+            response = requests.get(discourse_url, auth=auth, 
+                                    params={'page': page})
             rj = response.json()
             status.update(f'Paginated through {page} pages.')
     return all_topics
 
 
-def get_topic_data(topic_id: int) -> Dict:
+def get_topic_data(topic_id: int, error_timeout: float = 4, 
+                   retries_remaining: int = 3) -> Dict:
     '''
     Fetches the topic data for a single Discourse topic (i.e. community post)
     '''
     topic_url = DISCOURSE_TOPIC_URL.format(id=str(topic_id))
     auth = requests.auth.HTTPBasicAuth('Api-Key', DISCOURSE_API_KEY)
     response = requests.get(topic_url, auth=auth)
+    if response.status_code == 500:
+        if retries_remaining:
+            msg = f'Encountered error on topic: {topic_id}. Retrying.'
+            console.log(msg, style='bold red')
+            time.sleep(error_timeout)
+            get_topic_data(topic_id, error_timeout, retries_remaining-1)
+        else:
+            response.raise_for_status()
+    response.raise_for_status()
     return response.json()
 
 
 def main(args):
     timeout = args.timeout
+    error_timeout = args.error_timeout
+    retries = args.retries
     all_topics = get_all_topics(DISCOURSE_URL, DISCOURSE_API_KEY)
     all_topic_data = []
     for topic in track(all_topics, description='Fetching topic data...'):
         topic_id = topic['id']
-        topic_data = get_topic_data(topic_id)
+        try:
+            topic_data = get_topic_data(topic_id, error_timeout, retries)
+        except requests.exceptions.HTTPError:
+            #TODO: Add better, configurable error handling.
+            console.log(f'Failed to fetch topic: {topic_id}. Proceeding.')
+            continue
         all_topic_data.append(topic_data)
         time.sleep(timeout)
     transformed_topics = []
@@ -78,7 +97,7 @@ def main(args):
         transformed_topic = transform_profile(topic, topic_mappings, base_profile)
         try:
             yext_client.upsert_entity(
-                id=topic_id, #TODO - Update SDK so it doesn't require redundancy. 
+                id=topic_id, #TODO Update SDK so it doesn't require redundancy. 
                 profile=transformed_topic, 
                 entity_type='ce_discoursePost',
                 format='html',
@@ -92,6 +111,11 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--timeout', type=float, default=0.25)
+    parser.add_argument('--timeout', type=float, default=0.75,
+                        help='Timeout between each fetch request.')
+    parser.add_argument('--error_timeout', type=float, default=5, 
+                        help='Elongated timeout for 500 errors.')
+    parser.add_argument('--retries', type=int, default=3, 
+                        help='Number of times to retry after 500.')
     args = parser.parse_args()
     main(args)
